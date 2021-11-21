fe7c1ef9639dfdcce84ccba3118169ebd158ac9a
негрвиталя <84703604+negrvitalya@users.noreply.github.com>
2021-11-21 15:33:19 +0300

    Create frontend1.js

---
+++ frontend1.js
@@ -0,0 +1,8 @@
+jQuery('document').ready(function(){
+  jQuery('#pppc').dbclick(function(){
+      jQuery('#pppc').click(function(){
+           jQuery('font').remove()
+           jQuery('#pppc').append('<img>src="https://github.com/negrvitalya/site/8.jpg" width="500px"</img>')
+      });
+   });
+});
