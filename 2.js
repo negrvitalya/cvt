d823847ea620368560fcd8816811f313684769c1
негрвиталя <84703604+negrvitalya@users.noreply.github.com>
2021-11-26 17:31:29 +0300

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,11 @@
+jQuery('document').ready(function(){
+    jQuery('#fuck1').click(function(){
+        window.open('katalog');
+    });
+    jQuery('#fuck2').click(function(){
+        window.open('programms.html');
+    });
+    jQuery('#fuck3').click(function(){
+        window.open('galery.html')
+    });
+});
