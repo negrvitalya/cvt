4b53dcd67e38acdcea98e9a701056ac8ad060319
негрвиталя <84703604+negrvitalya@users.noreply.github.com>
2022-02-12 19:57:17 +0300

    Add files via upload

---
+++ vitalya/2.js
@@ -0,0 +1,27 @@
+jQuery('document').ready(function(){
+    var debug=true;
+    if(debug==false){
+    alert('если у тебя проблемы с сердцем - уходи отсюда\n8==D');}
+    function scream(){
+        let f = new Date()+300000;
+        while(new Date<f){}
+        jQuery('body').html('<img src="виталя.png"/>');
+        var au=new Audio();
+        au.src='Скример.mp3';
+        au.volume=1.0;
+        au.autoplay=true;
+        jQuery('title').html('слишком долго сидишь на сайте!');
+    }
+    jQuery('#b1').click(function(){
+        jQuery('#b2').trigger('click');
+    });
+    jQuery('#b2').click(function(){
+        jQuery('body').html('<img src="виталя.png"/>');
+        jQuery('body').css('backgroundColor','black');
+        var au=new Audio();
+        au.src='Скример.mp3';
+        au.volume=1.0;
+        au.autoplay=true;
+    });
+    //scream();
+});
